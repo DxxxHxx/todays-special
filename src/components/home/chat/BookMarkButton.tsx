0c23665c346d9bb8de7c9fe1d5c2cf5ced798f86
DxxxHxx <donghun1163@gmail.com>
2025-06-16 21:31:16 +0900

    refactor: 홈 즐겨찾기 버튼 리팩토링

--- src/components/home/chat/BookMarkButton.tsx
+++ src/components/home/chat/BookMarkButton.tsx
@@ -1,35 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/hooks/useAuth";
-import useBookmarkMenu from "@/hooks/useBookmarkMenu";
 import useBookmarkInfoStore from "@/store/bookmark/bookmarkInfoStore";
+import handleBookmark from "@/utils/bookmarkUtil";
 import triggerToast from "@/utils/toast";
 import { Star } from "lucide-react";
 import { useState } from "react";
 
 export default function BookMarkButton({ menu }: { menu: string }) {
-  const { id } = useBookmarkInfoStore();
-  const [isBookmarked, setIsBookmarked] = useState<null | boolean>(null);
-  const user = useAuth();
-
-  const bookmark = useBookmarkMenu(id!)!;
-
-  const handleBookmarkTriggerClick = async () => {
-    if (!user) {
-      return triggerToast("로그인 후 이용가능합니다.");
-    }
-    try {
-      const isBookmarked: boolean = (await bookmark()) ?? false;
-      setIsBookmarked(isBookmarked);
-
-      triggerToast(
-        `${menu}이(가) 즐겨찾기${
-          isBookmarked ? "에 추가되었습니다." : "에서 삭제되었습니다."
-        }`
-      );
-    } catch (e) {
-      alert(e);
-    }
-  };
+  const { handleBookmarkTriggerClick, isBookmarked } = useBookmark(menu);
   return (
     <Button className="save-button" onClick={handleBookmarkTriggerClick}>
       <Star
@@ -39,3 +17,31 @@ export default function BookMarkButton({ menu }: { menu: string }) {
     </Button>
   );
 }
+
+const useBookmark = (menu: string) => {
+  const { id } = useBookmarkInfoStore();
+  const [isBookmarked, setIsBookmarked] = useState<undefined | boolean>();
+  const user = useAuth();
+
+  const handleBookmarkTriggerClick = async () => {
+    if (!user) {
+      return triggerToast("로그인 후 이용가능합니다.");
+    }
+    try {
+      const isBookmarked = await handleBookmark(id!);
+      if (typeof isBookmarked === "boolean") {
+        setIsBookmarked(isBookmarked);
+        triggerToast(
+          `${menu}이(가) 즐겨찾기${
+            isBookmarked ? "에 추가되었습니다." : "에서 삭제되었습니다."
+          }`
+        );
+      } else {
+        triggerToast("요청에 실패했습니다.");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return { isBookmarked, handleBookmarkTriggerClick };
+};
